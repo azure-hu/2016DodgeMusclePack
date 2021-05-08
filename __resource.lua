f87689c780080cb8cf9de38f221fa1c01c0a98af
Maximilian Ring <3323241+maxgestic@users.noreply.github.com>
2021-05-08 17:25:24 +0100

    Added Vehicle Names lua

--- __resource.lua
+++ __resource.lua
@@ -14,3 +14,9 @@ data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
 data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
+
+client_script {
+
+    'vehicle_names.lua'
+    
+}
