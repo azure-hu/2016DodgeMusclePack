84a8c2e369a11e006137436b602c4c9badb948e3
Maximilian Ring <3323241+maxgestic@users.noreply.github.com>
2021-05-08 15:57:19 +0100

    Initial Commit

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+author 'SirChainsmokerGollum'
+description 'A FiveM Conversion of https://www.gta5-mods.com/vehicles/2015-dodge-challenger-add-on-stock-shaker-hellcat'
+version '1.0.0'
+
+files {
+    'data/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
