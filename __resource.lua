9b5134f769f42b534d474e6a2337a5359a9fa2a5
azure-hu <zollai@outlook.com>
2022-08-12 10:59:06 +0200

    - Included updated, and newly added parts from tk0wnz's v2.2a release (Release info copied from https://www.gta5-mods.com/vehicles/2015-dodge-challenger-add-on-stock-shaker-hellcat , v2.2 and v2.2a changes)   * Added Redeye badges   * Added new Hellcat bodyparts (hood, bumper, spoiler)   * Added new Widebody rim   * Improved taillight texture   * Fixed rear indicator lights   * Added embedded textures to all the add-on wheels   * Removed replace (tends to break after a few game updates)   * Paint:7 changes the halo DRL colors   * Added Carbon Black Aluminum rim   * Re-added the template - Changed some parts name to be more precise as it can be - Included tk0wnz's Dodge Rims Addon Pack, but instead of Sport Wheels category, all of them were moved to Muscle Wheels.

--- __resource.lua
+++ __resource.lua
@@ -13,7 +13,7 @@ data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/contentunlocks.meta'
 
 client_script {
 
