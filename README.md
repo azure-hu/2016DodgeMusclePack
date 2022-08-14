a3102037b702b92050c33b6635c3c7b0349467dc
azure-hu <zollai@outlook.com>
2022-08-14 17:20:20 +0200

    - changed parts names in vehicle_names.lua to use more precise names, and an order was set up - new addition: Dodge Charger 6.4/6.2SC V8 Engine Sound from https://www.gta5-mods.com/vehicles/dodge-charger-6-4-6-2sc-v8-engine-sound-oiv-add-on-fivem-sound

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 A FiveM Conversion of https://www.gta5-mods.com/vehicles/2015-dodge-challenger-add-on-stock-shaker-hellcat
 Based on maxgestic's work from https://github.com/maxgestic/2016DodgeChallenger
 
+New addition: Dodge Charger 6.4/6.2SC V8 Engine Sound from https://www.gta5-mods.com/vehicles/dodge-charger-6-4-6-2sc-v8-engine-sound-oiv-add-on-fivem-sound
 #Installation
 
 1. Download latest file from https://github.com/maxgestic/2016DodgeChallenger/releases/
 2. Drag the 16challenger folder from ZIP file into your resources folder
 3. Add "ensure 16challenger" to your server.cfg file
 4. Profit
+
+To use custom sounds, edit the vehicles.meta it is defined in, and replace this line with either;
+
+<audioNameHash>npolchar</audioNameHash> (6.4 NA; police version; addon siren setup)
+<audioNameHash>npolcharciv</audioNameHash> (6.4 NA; civillian version; no sirens, with civillian horn)
+<audioNameHash>npolchar2</audioNameHash> (6.2 SC; police version; addon siren setup)
+<audioNameHash>npolchar2civ</audioNameHash> (6.2 SC; civillian version; no sirens, with civillian horn)
 
 #Usage
 Spawn car in FiveM with name 16challenger
--- __resource.lua
+++ __resource.lua
@@ -1,19 +1,24 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
-author 'SirChainsmokerGollum'
-description 'A FiveM Conversion of https://www.gta5-mods.com/vehicles/2015-dodge-challenger-add-on-stock-shaker-hellcat'
+author 'SirChainsmokerGollum (modified by Azur3)'
+description 'A FiveM Conversion of https://www.gta5-mods.com/vehicles/2015-dodge-challenger-add-on-stock-shaker-hellcat with audio from https://www.gta5-mods.com/vehicles/dodge-charger-6-4-6-2sc-v8-engine-sound-oiv-add-on-fivem-sound'
 version '1.0.0'
 
 files {
+	'audioconfig/dlc_npolchar/*.dat151.rel',
+	'audioconfig/dlc_npolchar/*.dat54.rel',
+	'sfx/dlc_npolchar/*.awc',
     'data/**/*.meta'
 }
-
-data_file 'HANDLING_FILE' 'data/**/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/contentunlocks.meta'
+	data_file 'AUDIO_GAMEDATA' 'audioconfig/dlc_npolchar/npolchar_game.dat'
+	data_file 'AUDIO_SOUNDDATA' 'audioconfig/dlc_npolchar/npolchar_sounds.dat'
+	data_file 'AUDIO_WAVEPACK' 'sfx/dlc_npolchar'
+	data_file 'HANDLING_FILE' 'data/**/handling.meta'
+	data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+	data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+	data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+	data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+	data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/contentunlocks.meta'
 
 client_script {
 
